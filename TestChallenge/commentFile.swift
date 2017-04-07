6d225f3fec276dc4585abea516ad826fa8584aff
nicholaslee <nicholaslee@nicholaslees-MacBook-Pro.local>
2017-04-07 17:29:44 +0800

    comment file added

---
+++ TestChallenge/commentFile.swift
@@ -0,0 +1,41 @@
+//
+//  commentFile.swift
+//  TestChallenge
+//
+//  Created by nicholaslee on 07/04/2017.
+//  Copyright © 2017 nicholaslee. All rights reserved.
+//
+
+import Foundation
+
+
+
+//Nicholas comments
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Suffian comments
+
